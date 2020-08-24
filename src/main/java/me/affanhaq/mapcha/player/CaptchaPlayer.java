0e538d6cea56d12934f88d10d7f607a293f4aa8c
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2020-08-24 13:03:33 -0400

    Fixed some spacing, elements were going off the map

--- src/main/java/me/affanhaq/mapcha/player/CaptchaPlayer.java
+++ src/main/java/me/affanhaq/mapcha/player/CaptchaPlayer.java
@@ -62,17 +62,17 @@ public class CaptchaPlayer {
 
         String sTries = "Tries Left: ";
         g.setColor(Color.WHITE);
-        g.drawString(sTries, (int) ((image.getWidth() - g.getFontMetrics().getStringBounds(sTries, g).getWidth()) / 2), 45);
+        g.drawString(sTries, (int) ((image.getWidth() - g.getFontMetrics().getStringBounds(sTries, g).getWidth()) / 4), 45);
         g.setColor((Mapcha.Config.tries - tries) == 1 ? Color.RED : Color.GREEN);
-        g.drawString(String.valueOf((Mapcha.Config.tries - tries)), (int) (((image.getWidth() - g.getFontMetrics().getStringBounds(sTries, g).getWidth()) / 2) + g.getFontMetrics().getStringBounds(sTries, g).getWidth() + 2), 45);
+        g.drawString(String.valueOf((Mapcha.Config.tries - tries)), (int) (((image.getWidth() - g.getFontMetrics().getStringBounds(sTries, g).getWidth()) / 2) + g.getFontMetrics().getStringBounds(sTries, g).getWidth() + 4), 45);
 
         String sTime = "Time Left: ";
         g.setColor(Color.WHITE);
-        g.drawString(sTime, (int) ((image.getWidth() - g.getFontMetrics().getStringBounds(sTime, g).getWidth()) / 2), 55);
+        g.drawString(sTime, (int) ((image.getWidth() - g.getFontMetrics().getStringBounds(sTime, g).getWidth()) / 4), 55);
         g.setColor((timeLimit * 1000) - (System.currentTimeMillis() - lastTime) == 1000 ? Color.RED : Color.GREEN);
-        g.drawString(new SimpleDateFormat("ss").format((timeLimit * 1000) - (System.currentTimeMillis() - lastTime)) + " sec", (int) (((image.getWidth() - g.getFontMetrics().getStringBounds(sTime, g).getWidth()) / 2) + g.getFontMetrics().getStringBounds(sTime, g).getWidth() + 2), 55);
+        g.drawString(new SimpleDateFormat("ss").format((timeLimit * 1000) - (System.currentTimeMillis() - lastTime)) + " sec", (int) (((image.getWidth() - g.getFontMetrics().getStringBounds(sTime, g).getWidth()) / 4) + g.getFontMetrics().getStringBounds(sTime, g).getWidth() + 2), 55);
 
-        g.setFont(new Font("Arial", Font.BOLD, 40));
+        g.setFont(new Font("Arial", Font.BOLD, 35));
         g.setColor(Color.WHITE);
         g.drawString(captcha, (int) ((image.getWidth() - g.getFontMetrics().getStringBounds(captcha, g).getWidth()) / 2), 105);
 
