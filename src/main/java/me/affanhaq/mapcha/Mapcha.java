61df6c5f4d11534808aa88a37cd12e9e2a5ea4f3
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2021-03-30 16:45:47 -0400

    end my life

--- src/main/java/me/affanhaq/mapcha/Mapcha.java
+++ src/main/java/me/affanhaq/mapcha/Mapcha.java
@@ -2,12 +2,12 @@ package me.affanhaq.mapcha;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import me.affanhaq.keeper.Keeper;
+import me.affanhaq.keeper.data.ConfigValue;
 import me.affanhaq.mapcha.handlers.CaptchaHandler;
 import me.affanhaq.mapcha.handlers.MapHandler;
 import me.affanhaq.mapcha.handlers.PlayerHandler;
 import me.affanhaq.mapcha.player.CaptchaPlayerManager;
-import me.ihaq.keeper.Keeper;
-import me.ihaq.keeper.data.ConfigValue;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
